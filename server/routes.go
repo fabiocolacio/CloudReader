4bc17245b7e5e2db7b8e8db4485cc5b0dff57208
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-11-19 17:10:08 -0800

    Create routes handler

--- server/routes.go
+++ server/routes.go
@@ -22,6 +22,49 @@ func ListenAndServe() error {
 }
 
 func routeHandler(res http.ResponseWriter, req *http.Request) {
-    res.Write([]byte("<h1>Hello World</h1>"))
+    path := req.URL.Path
+
+    switch path {
+    case "/":
+        // TODO: Show home page
+
+    case "/login":
+        // TODO: If it is a GET request, send login HTML page
+        // If it is a POST request, log them in or tell them if there is
+        // na error
+
+    case "/register":
+        // TODO: If it is a GET request, send the register HTML page
+        //If it is a POST request, add user to database or send back error
+
+    case "/library":
+        // TODO: Send HTML for the user's library
+        // Users can logout, upload book, or read book
+
+    case "/logout":
+        // TODO: Log user out and send back to home page
+
+    case "/upload":
+        // TODO: Send HTML to the upload page if it is a GET request.
+        // If it is a POST request, add the book to the database, etc.
+        // Send user to Library.
+
+    case "/read":
+        // TODO: Send HTML to read the book
+
+    default:
+        res.Write([]byte("<h1>Hello World</h1>"))
+    }
+}
+
+func ReadBody(req *http.Request) (body []byte, err error) {
+    body = make([]byte, req.ContentLength) 
+    read, err := req.Body.Read(body);
+
+    if int64(read) == req.ContentLength {
+        err = nil
+    }
+
+    return body, err
 }
 
